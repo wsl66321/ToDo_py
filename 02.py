7bd802817d9bc3c6b772430ddf98cfc431b70271
wsl <1030005627@qq.com>
2020-04-03 20:17:02 +0800

    2020年4月3日20:16:53

--- 02.py
+++ 02.py
@@ -9,37 +9,53 @@ class FirstMainWin(QMainWindow):
         super(FirstMainWin,self).__init__(parent)
 
         self.setWindowTitle("双端同步备忘录系统")
-        self.setStyleSheet('QMainWindow{border-image:url(c:/picture/w.png)}')
-        self.resize(300,500)
+        self.setStyleSheet('QMainWindow{border-image:url(res/loginbg1.png)}')
+        self.resize(360,640)
+        self.setMinimumSize(360,640)
+        self.setMaximumSize(360,640)
         self.initUI()
 
     def center(self):
         screen = QDesktopWidget().screenGeometry()
         size = self.geometry()
         newLeft = (screen.width()-size.width())/2
-        newRight = (screen.height() - size.height()) / 2
+        newRight = (screen.height() - size.height()) /2
         self.move(newLeft,newRight)
 
     def initUI(self):
-        label1 = QLabel(self)
-        label1.setText('账号')
-        label1.setStyleSheet("font-size:20px;")
-        label1.move(70,100)
+        # label1 = QLabel(self)
+        # label1.setText('账号')
+        # label1.setStyleSheet("font-size:20px;")
+        # label1.move(70,100)
 
-        edit1 = QTextEdit(self)
-        edit1.move(120,100)
+        # image=QImage(self)
+        # image
 
-        label2 = QLabel(self)
-        label2.setText('密码')
-        label2.setStyleSheet("font-size:20px;")
-        label2.move(70, 200)
+        image = QLabel(self)
+        image.setPixmap(QPixmap('res/login.png'))
+        image.move(80, 200)
 
-        edit1 = QTextEdit(self)
-        edit1.move(120, 200)
+        edit1 = QLineEdit(self)
+        edit1.setPlaceholderText("账号")
+        edit1.move(80,100)
+        edit1.setMinimumWidth(200)
+
+
+        # label2 = QLabel(self)
+        # label2.setText('密码')
+        # label2.setStyleSheet("font-size:20px;")
+        # label2.move(70, 200)
+
+        edit2 = QLineEdit(self)
+        edit2.setMinimumWidth(200)
+        edit2.move(80, 200)
+        # edit2.resize(200,25)
+        edit2.setPlaceholderText("密码")
+
 
         label3 = QLabel(self)
         label3.setText('登陆')
-        label3.setStyleSheet("font-size:20px;")
+        label3.setStyleSheet("font-size:40px;color:#fff")
         label3.move(130, 350)
 
 class SecondMainWin(QDialog):
